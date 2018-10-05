701fdd450d31a48f9c911ace0d525212a623d3bd
Doug <doug@bonniechef.com>
2018-10-05 14:18:39 +0100

    Set StripeID Length to 180

--- Model/AbstractCardModel.php
+++ Model/AbstractCardModel.php
@@ -10,149 +10,186 @@ use BonnieDoug\StripeBundle\Annotation\StripeObjectParam;
 abstract class AbstractCardModel extends StripeModel
 {
     /**
-     * @StripeObjectParam(name="address_city")
-     *
+     * String representing the object’s type. Objects of the same type share the same value.
      * @var string
+     */
+    protected $object;
+
+    /**
+     * The account this card belongs to. This attribute will not be in the card object if the card belongs to a customer or recipient instead.
+     * @var string | null
+     */
+    protected $account;
+
+    /**
+     * City/District/Suburb/Town/Village.
+     * @StripeObjectParam(name="address_city")
+     * @var string | null
      */
     protected $addressCity;
 
     /**
+     * Billing address country, if provided when creating card.
      * @StripeObjectParam(name="address_country")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressCountry;
 
     /**
+     * Address line 1 (Street address/PO Box/Company name).
      * @StripeObjectParam(name="address_line1")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressLine1;
 
     /**
+     * If address_line1 was provided, results of the check: pass, fail, unavailable, or unchecked.
      * @StripeObjectParam(name="address_line1_check")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressLine1Check;
 
     /**
+     * Address line 2 (Apartment/Suite/Unit/Building).
      * @StripeObjectParam(name="address_line2")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressLine2;
 
     /**
+     * State/County/Province/Region.
      * @StripeObjectParam(name="address_state")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressState;
 
     /**
+     * ZIP or postal code.
      * @StripeObjectParam(name="address_zip")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressZip;
 
     /**
+     * If address_zip was provided, results of the check: pass, fail, unavailable, or unchecked.
      * @StripeObjectParam(name="address_zip_check")
-     *
-     * @var string
+     * @var string | null
      */
     protected $addressZipCheck;
 
     /**
+     * A set of available payout methods for this card. Will be either ["standard"] or ["standard", "instant"]. Only values from this set should be passed as the method when creating a transfer.
+     * @var array | null
+     */
+    protected $avaliablePayoutMethods;
+
+    /**
+     * Card brand. Can be American Express, Diners Club, Discover, JCB, MasterCard, UnionPay, Visa, or Unknown.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $brand;
 
     /**
+     * Two-letter ISO code representing the country of the card. You could use this attribute to get a sense of the international breakdown of cards you’ve collected.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $country;
 
     /**
+     * Three-letter ISO code for currency. Only applicable on accounts (not customers or recipients). The card can be used as a transfer destination for funds in this currency.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
+     */
+    protected $currency;
+
+    /**
+     * The customer that this card belongs to. This attribute will not be in the card object if the card belongs to an account or recipient instead.
+     * @StripeObjectParam
+     * @var string | null
      */
     protected $customer;
 
     /**
+     * If a CVC was provided, results of the check: pass, fail, unavailable, or unchecked.
      * @StripeObjectParam(name="cvc_check")
-     *
-     * @var string
+     * @var string | null
      */
     protected $cvcCheck;
 
     /**
+     * Only applicable on accounts (not customers or recipients). This indicates whether this card is the default external account for its currency.
+     * @var string | null
+     */
+    protected $defaultForCurrency;
+
+    /**
+     * (For tokenized numbers only.) The last four digits of the device account number.
      * @StripeObjectParam(name="dynamic_last4")
-     *
-     * @var string
+     * @var string | null
      */
     protected $dynamicLast4;
 
     /**
+     * Two-digit number representing the card’s expiration month.
      * @StripeObjectParam(name="exp_month")
-     *
-     * @var int
+     * @var integer | null
      */
     protected $expMonth;
 
     /**
+     * Four-digit number representing the card’s expiration year.
      * @StripeObjectParam(name="exp_year")
-     *
-     * @var int
+     * @var integer | null
      */
     protected $expYear;
 
     /**
+     * Uniquely identifies this particular card number. You can use this attribute to check whether two customers who’ve signed up with you are using the same card number, for example.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $fingerprint;
 
     /**
+     * Card funding type. Can be credit, debit, prepaid, or unknown.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $funding;
 
     /**
+     * The last four digits of the card.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $last4;
 
     /**
+     * Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
      * @StripeObjectParam
-     *
-     * @var string
-     */
-    protected $name;
-
-    /**
-     * @StripeObjectParam
-     *
-     * @var array
+     * @var string | null
      */
     protected $metadata;
 
     /**
+     * Cardholder name.
+     * @StripeObjectParam
+     * @var string | null
+     */
+    protected $name;
+
+    /**
+     * The recipient that this card belongs to. This attribute will not be in the card object if the card belongs to a customer or account instead.
+     * @var string | null
+     */
+    protected $recipient;
+
+    /**
+     * If the card number is tokenized, this is the method that was used. Can be apple_pay or android_pay.
      * @StripeObjectParam(name="tokenization_method")
-     *
-     * @var string
+     * @var string | null
      */
     protected $tokenizationMethod;
 
@@ -573,6 +610,114 @@ abstract class AbstractCardModel extends StripeModel
     {
         $this->tokenizationMethod = $tokenizationMethod;
 
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * @param string $object
+     * @return AbstractCardModel
+     */
+    public function setObject($object)
+    {
+        $this->object = $object;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * @param null|string $account
+     * @return AbstractCardModel
+     */
+    public function setAccount($account)
+    {
+        $this->account = $account;
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getAvaliablePayoutMethods()
+    {
+        return $this->avaliablePayoutMethods;
+    }
+
+    /**
+     * @param array|null $avaliablePayoutMethods
+     * @return AbstractCardModel
+     */
+    public function setAvaliablePayoutMethods($avaliablePayoutMethods)
+    {
+        $this->avaliablePayoutMethods = $avaliablePayoutMethods;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * @param null|string $currency
+     * @return AbstractCardModel
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDefaultForCurrency()
+    {
+        return $this->defaultForCurrency;
+    }
+
+    /**
+     * @param null|string $defaultForCurrency
+     * @return AbstractCardModel
+     */
+    public function setDefaultForCurrency($defaultForCurrency)
+    {
+        $this->defaultForCurrency = $defaultForCurrency;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getRecipient()
+    {
+        return $this->recipient;
+    }
+
+    /**
+     * @param null|string $recipient
+     * @return AbstractCardModel
+     */
+    public function setRecipient($recipient)
+    {
+        $this->recipient = $recipient;
         return $this;
     }
 }
--- Model/AbstractChargeModel.php
+++ Model/AbstractChargeModel.php
@@ -6,178 +6,250 @@ use BonnieDoug\StripeBundle\Annotation\StripeObjectParam;
 
 abstract class AbstractChargeModel extends StripeModel
 {
-    /**
-     * @StripeObjectParam
-     *
-     * @var int
-     */
-    protected $amount;
 
     /**
-     * @StripeObjectParam(name="amount_refunded")
-     *
+     * String representing the object’s type. Objects of the same type share the same value.
+     * @var string | null
+     */
+    protected $object = "charge";
+
+    /**
+     * A positive integer in the smallest currency unit (e.g., 100 cents to charge $1.00 or 100 to charge ¥100, a zero-decimal currency) representing how much to charge. The minimum amount is $0.50 US or equivalent in charge currency.
      * @var int
+     * @Groups({"post"})
+     * @StripeObjectParam
+     * @Assert\NotBlank(groups={"post"})
+     */
+    protected $amount = 0;
+
+    /**
+     * Amount in cents refunded (can be less than the amount attribute on the charge if a partial refund was issued).
+     * @StripeObjectParam(name="amount_refunded")
+     * @var string | null
      */
     protected $amountRefunded;
 
     /**
+     * ID of the Connect application that created the charge.
+     * @var string | null
+     */
+    protected $application;
+
+    /**
+     * The application fee (if any) for the charge. See the Connect documentation for details.
+     * @var string | null
+     */
+    protected $applicationFee;
+
+    /**
+     * ID of the balance transaction that describes the impact of this charge on your account balance (not including refunds or disputes).
      * @StripeObjectParam(name="balance_transaction")
-     *
-     * @var string
+     * @var  string | null
      */
     protected $balanceTransaction;
 
     /**
+     * If the charge was created without capturing, this Boolean represents whether it is still uncaptured or has since been captured.
      * @StripeObjectParam
-     *
-     * @var bool
+     * @var boolean
      */
-    protected $captured;
+    protected $captured = false;
 
     /**
+     * Time at which the object was created. Measured in seconds since the Unix epoch.
      * @StripeObjectParam
-     *
-     * @var int
+     * @var integer
      */
     protected $created;
 
     /**
+     * Three-letter ISO currency code, in lowercase. Must be a supported currency.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var  string | null
      */
-    protected $currency;
+    protected $currency = "GBP";
 
     /**
+     * ID of the customer this charge is for if one exists.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $customer;
 
     /**
+     * An arbitrary string attached to the object. Often useful for displaying to users.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $description;
 
     /**
-     * @var string
+     * The account (if any) the charge was made on behalf of, with an automatic transfer. See the Connect documentation for details.
+     * @StripeObjectParam
+     * @var string | null
+     */
+    protected $destination;
+
+    /**
+     * Details about the dispute if the charge has been disputed.
+     * @StripeObjectParam
+     * @var string | null
      */
     protected $dispute;
 
     /**
+     * Error code explaining reason for charge failure if available (see the errors section for a list of codes).
      * @StripeObjectParam(name="failure_code")
-     *
-     * @var string
+     * @var string | null
      */
     protected $failureCode;
 
     /**
+     * Message to user further explaining reason for charge failure if available.
      * @StripeObjectParam(name="failure_message")
-     *
-     * @var string
+     * @var string | null
      */
     protected $failureMessage;
 
     /**
+     * Hash with information on fraud assessments for the charge. Assessments reported by you have the key user_report and, if set, possible values of safe and fraudulent. Assessments from Stripe have the key stripe_report and, if set, the value fraudulent.
      * @StripeObjectParam(name="fraud_details")
-     *
-     * @var array
+     * @var array | null
      */
     protected $fraudDetails;
 
     /**
+     * ID of the invoice this charge is for if one exists.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $invoice;
 
     /**
+     * Has the value true if the object exists in live mode or the value false if the object exists in test mode.
      * @StripeObjectParam
-     *
-     * @var bool
+     * @var boolean
      */
-    protected $livemode;
+    protected $livemode = true;
 
     /**
+     * Set of key-value pairs that you can attach to an object. This can be useful for storing additional information about the object in a structured format.
      * @StripeObjectParam
-     *
-     * @var array
+     * @var array | null
      */
     protected $metadata;
 
     /**
+     * The account (if any) the charge was made on behalf of without triggering an automatic transfer. See the Connect documentation for details.
+     * @StripeObjectParam(name="on_behlaf_of")
+     * @var string | null
+     */
+    protected $onBehalfOf;
+
+    /**
+     * ID of the order this charge is for if one exists.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $order;
 
     /**
+     * Details about whether the payment was accepted, and why. See understanding declines for details.
      * @StripeObjectParam
-     *
-     * @var array
+     * @var array | null
      */
     protected $outcome;
 
     /**
+     * true if the charge succeeded, or was successfully authorized for later capture.
      * @StripeObjectParam
-     *
-     * @var bool
+     * @var boolean
      */
-    protected $paid;
+    protected $paid = false;
 
     /**
+     * ID of the PaymentIntent associated with this charge, if one exists.
+     * @StripeObjectParam(name="payment_intent")
+     * @var string | null
+     */
+    protected $paymentIntent;
+
+    /**
+     * This is the email address that the receipt for this charge was sent to.
      * @StripeObjectParam(name="receipt_email")
-     *
-     * @var string
+     * @var string | null
      */
     protected $receiptEmail;
 
     /**
+     * This is the transaction number that appears on email receipts sent for this charge. This attribute will be null until a receipt has been sent.
      * @StripeObjectParam(name="receipt_number")
-     *
-     * @var string
+     * @var string | null
      */
     protected $receiptNumber;
 
     /**
+     * Whether the charge has been fully refunded. If the charge is only partially refunded, this attribute will still be false.
      * @StripeObjectParam
-     *
-     * @var bool
+     * @var boolean
      */
-    protected $refunded;
+    protected $refunded = false;
 
     /**
+     * A list of refunds that have been applied to the charge.
      * @StripeObjectParam
-     *
-     * @var array
+     * @var array | null
+     */
+    protected $refunds;
+
+    /**
+     * ID of the review associated with this charge if one exists.
+     * @StripeObjectParam
+     * @var string | null
+     */
+    protected $review;
+
+    /**
+     * Shipping information for the charge.
+     * @StripeObjectParam
+     * @var array | null
      */
     protected $shipping;
 
     /**
+     * For most Stripe users, the source of every charge is a credit or debit card. This hash is then the card object describing that card.
      * @StripeObjectParam(embeddedId="id")
-     *
-     * @var string
+     * @var array | null
      */
     protected $source;
 
     /**
+     * The transfer ID which created this charge. Only present if the charge came from another Stripe account. See the Connect documentation for details.
+     * @StripeObjectParam(name="source_transfer")
+     * @var string | null
+     */
+    protected $sourceTransfer;
+
+    /**
+     * Extra information about a charge. This will appear on your customer’s credit card statement. It must contain at least one letter.
      * @StripeObjectParam(name="statement_descriptor")
-     *
-     * @var string
+     * @var string | null
      */
     protected $statementDescriptor;
 
     /**
+     * The status of the payment is either succeeded, pending, or failed.
      * @StripeObjectParam
-     *
-     * @var string
+     * @var string | null
      */
     protected $status;
+
+    /**
+     * ID of the transfer to the destination account (only applicable if the charge was created using the destination parameter).
+     * @StripeObjectParam
+     * @var string | null
+     */
+    protected $transfer;
 
     /**
      * @return int
@@ -678,4 +750,185 @@ abstract class AbstractChargeModel extends StripeModel
 
         return $this;
     }
+
+    /**
+     * @return null|string
+     */
+    public function getObject()
+    {
+        return $this->object;
+    }
+
+    /**
+     * @param null|string $object
+     * @return AbstractChargeModel
+     */
+    public function setObject($object)
+    {
+        $this->object = $object;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getApplication()
+    {
+        return $this->application;
+    }
+
+    /**
+     * @param null|string $application
+     * @return AbstractChargeModel
+     */
+    public function setApplication($application)
+    {
+        $this->application = $application;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getApplicationFee()
+    {
+        return $this->applicationFee;
+    }
+
+    /**
+     * @param null|string $applicationFee
+     * @return AbstractChargeModel
+     */
+    public function setApplicationFee($applicationFee)
+    {
+        $this->applicationFee = $applicationFee;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDestination()
+    {
+        return $this->destination;
+    }
+
+    /**
+     * @param null|string $destination
+     * @return AbstractChargeModel
+     */
+    public function setDestination($destination)
+    {
+        $this->destination = $destination;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getOnBehalfOf()
+    {
+        return $this->onBehalfOf;
+    }
+
+    /**
+     * @param null|string $onBehalfOf
+     * @return AbstractChargeModel
+     */
+    public function setOnBehalfOf($onBehalfOf)
+    {
+        $this->onBehalfOf = $onBehalfOf;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getPaymentIntent()
+    {
+        return $this->paymentIntent;
+    }
+
+    /**
+     * @param null|string $paymentIntent
+     * @return AbstractChargeModel
+     */
+    public function setPaymentIntent($paymentIntent)
+    {
+        $this->paymentIntent = $paymentIntent;
+        return $this;
+    }
+
+    /**
+     * @return array|null
+     */
+    public function getRefunds()
+    {
+        return $this->refunds;
+    }
+
+    /**
+     * @param array|null $refunds
+     * @return AbstractChargeModel
+     */
+    public function setRefunds($refunds)
+    {
+        $this->refunds = $refunds;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getReview()
+    {
+        return $this->review;
+    }
+
+    /**
+     * @param null|string $review
+     * @return AbstractChargeModel
+     */
+    public function setReview($review)
+    {
+        $this->review = $review;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getSourceTransfer()
+    {
+        return $this->sourceTransfer;
+    }
+
+    /**
+     * @param null|string $sourceTransfer
+     * @return AbstractChargeModel
+     */
+    public function setSourceTransfer($sourceTransfer)
+    {
+        $this->sourceTransfer = $sourceTransfer;
+        return $this;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getTransfer()
+    {
+        return $this->transfer;
+    }
+
+    /**
+     * @param null|string $transfer
+     * @return AbstractChargeModel
+     */
+    public function setTransfer($transfer)
+    {
+        $this->transfer = $transfer;
+        return $this;
+    }
+
 }
